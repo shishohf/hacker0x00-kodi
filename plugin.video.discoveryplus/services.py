745567fa0569700d672c28ef19131fddc6c42b90
shishohf <shishohf@protonmail.com>
2023-11-01 13:03:44 -0400

    Fix

--- plugin.video.discoveryplus/services.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import threading
-
-import resources.services.realmservice as realmservice
-
-realmservice_th = threading.Thread(target=realmservice.main)
-
-realmservice_th.start()
